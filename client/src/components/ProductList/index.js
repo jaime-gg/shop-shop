059f0b66c36f75d845cfe98efee1e11d9c3bd2ef
jester <jaimebuddy98@gmail.com>
2022-04-15 17:16:48 -0500

    added use store context and global context

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
+
 
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
