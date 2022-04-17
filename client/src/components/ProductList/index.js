3c03a3b0e2dabaee2a690be4ec350fd909098392
jester <jaimebuddy98@gmail.com>
2022-04-17 14:37:14 -0500

    initgrated idbPromise

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -7,6 +7,7 @@ import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
 
 function ProductList() {
   // const { loading, data } = useQuery(QUERY_PRODUCTS);
@@ -30,13 +31,20 @@ function ProductList() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
+    // if there's data to be stored
     if (data) {
+      // let's store it in the global state object
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+  
+      // but let's also take each product and save it to IndexedDB using the helper function 
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
@@ -45,6 +53,7 @@ function ProductList() {
 
     return state.products.filter(product => product.category._id === currentCategory);
   }
+
 
   return (
     <div className="my-2">
