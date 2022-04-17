daa7cfd4c2af801633c66c4592db5100c7d5e8af
jester <jaimebuddy98@gmail.com>
2022-04-17 14:34:28 -0500

    setup idb helper function

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -4,3 +4,72 @@ export function pluralize(name, count) {
   }
   return name + 's'
 }
+
+
+
+export function ibdPromise(storeName, method, object) {
+  return new Promise((resolve, reject) => {
+    // open connection to the database 'shop-shop' with the version of one
+    const request = window.indexedDB.open('shop-shop', 1)
+
+    //create variable to hold reference to the database, transaction (tx), and object store.
+    let db, tx, store;
+
+    // if version has changed (or first time using the fatabase), run this method and create the three object stores. 
+    request.onupgradeneeded = function (e) {
+      const db = request.result;
+      // create object stores for each tyoe of data and set "primary" key index to be the '_id' of the data
+      db.createObjectStore('products', { keyPath: '_id' });
+      db.createObjectStore('categories', { keyPath: '_id' });
+      db.createObjectStore('cart', { keyPath: '_id' });
+    };
+
+    // handle any errors with connecting 
+    request.onerror = function (e) {
+      console.log('there was an error ;-;')
+    }
+
+    // on database open success
+    request.onsuccess = function (e) {
+      // save a refrence of the database to the 'db' variable
+      db = request.result;
+      //open a transaction do whatever we pass into 'storeName' (must match one of the object store names)
+      tx = db.transaction(storeName, 'readwrite');
+      //save a reference ro that object store 
+      store = tx.objectStore(storeName)
+
+      // on error let us know
+      db.onerror = function (e) {
+        console.log('error', e);
+      };
+
+      switch (method) {
+        case 'put':
+          store.put(object);
+          result(object);
+          break;
+
+        case 'get':
+          const all = store.getAll();
+          all.onsuccess = function () {
+            resolve(all.result);
+          };
+          break;
+
+        case 'delete':
+          store.delete(object._id);
+          break;
+
+        default:
+          console.log('No valid method');
+          break;
+      }
+
+      // when the transaction is complete, close the connection
+      tx.oncomplete = function () {
+        db.close();
+      };
+    }
+
+  })
+}
